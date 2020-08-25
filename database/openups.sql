255b35cc4dc115106387fbf21e3bae3a9fcae219
Atharva Deshpande <34884015+AD1310@users.noreply.github.com>
2020-08-25 20:04:13 +0530

    db file

--- database/openups.sql
+++ database/openups.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 22, 2020 at 05:57 AM
+-- Generation Time: Aug 25, 2020 at 04:31 PM
 -- Server version: 10.1.38-MariaDB
 -- PHP Version: 7.3.3
 
@@ -25,14 +25,32 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `buyers`
+-- Table structure for table `adimgs`
 --
 
-CREATE TABLE `buyers` (
-  `pid` bigint(20) UNSIGNED NOT NULL,
-  `sid` bigint(20) UNSIGNED NOT NULL,
-  `bid` bigint(20) UNSIGNED NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+CREATE TABLE `adimgs` (
+  `aid` int(10) NOT NULL,
+  `img` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+-- Error reading data for table openups.adimgs: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `openups`.`adimgs`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ads`
+--
+
+CREATE TABLE `ads` (
+  `aid` int(10) NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `description` varchar(500) NOT NULL,
+  `brand` varchar(50) NOT NULL,
+  `price` int(20) NOT NULL,
+  `tperiod` int(5) NOT NULL,
+  `visibility` int(5) NOT NULL,
+  `uid` int(10) NOT NULL,
+  `scid` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -41,138 +59,23 @@ CREATE TABLE `buyers` (
 --
 
 CREATE TABLE `cat` (
-  `cid` bigint(20) UNSIGNED NOT NULL,
-  `cname` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Dumping data for table `cat`
---
-
-INSERT INTO `cat` (`cid`, `cname`) VALUES
-(1, 'xyz');
+  `cid` int(10) NOT NULL,
+  `cname` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `chat`
+-- Table structure for table `locations`
 --
 
-CREATE TABLE `chat` (
-  `sid` bigint(20) UNSIGNED NOT NULL,
-  `rid` bigint(20) UNSIGNED NOT NULL,
-  `message` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `failed_jobs`
---
-
-CREATE TABLE `failed_jobs` (
-  `id` bigint(20) UNSIGNED NOT NULL,
-  `connection` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `queue` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
-  `exception` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
-  `failed_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `fav`
---
-
-CREATE TABLE `fav` (
-  `pid` bigint(20) UNSIGNED NOT NULL,
-  `uid` bigint(20) UNSIGNED NOT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `migrations`
---
-
-CREATE TABLE `migrations` (
-  `id` int(10) UNSIGNED NOT NULL,
-  `migration` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `batch` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Dumping data for table `migrations`
---
-
-INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
-(1, '2014_10_12_000000_create_users_table', 1),
-(2, '2019_08_19_000000_create_failed_jobs_table', 2),
-(3, '2020_08_09_071502_create_chat_table', 3),
-(4, '2020_08_09_072340_create_cat_table', 4),
-(5, '2020_08_09_072350_create_scat_table', 5),
-(6, '2020_08_09_062831_create_posts_table', 6),
-(7, '2020_08_09_072855_create_postimg_table', 7),
-(8, '2020_08_09_073230_create_buyers_table', 8),
-(9, '2020_08_09_073956_create_fav_table', 9);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `password_resets`
---
-
-CREATE TABLE `password_resets` (
-  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `created_at` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `postimg`
---
-
-CREATE TABLE `postimg` (
-  `pid` bigint(20) UNSIGNED NOT NULL,
-  `pimg` text COLLATE utf8mb4_unicode_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `posts`
---
-
-CREATE TABLE `posts` (
-  `pid` bigint(20) UNSIGNED NOT NULL,
-  `pname` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `title` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `brand` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `description` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `price` int(11) NOT NULL,
-  `ploc` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `tperiod` int(11) NOT NULL,
-  `visibility` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `uid` bigint(20) UNSIGNED NOT NULL,
-  `cid` bigint(20) UNSIGNED NOT NULL,
-  `scid` bigint(20) UNSIGNED NOT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Dumping data for table `posts`
---
-
-INSERT INTO `posts` (`pid`, `pname`, `title`, `brand`, `description`, `price`, `ploc`, `tperiod`, `visibility`, `uid`, `cid`, `scid`, `created_at`, `updated_at`) VALUES
-(1, 'Mobile', 'My Mobile on Sale', 'Samsung', '2 yrs old, black colored', 5000, 'Chinchwad', 2, '1', 1, 1, 1, '2020-08-20 00:35:47', '2020-08-20 00:35:47');
+CREATE TABLE `locations` (
+  `aid` int(10) NOT NULL,
+  `state` varchar(20) NOT NULL,
+  `city` varchar(20) NOT NULL,
+  `area` varchar(20) NOT NULL,
+  `landmark` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -181,16 +84,10 @@ INSERT INTO `posts` (`pid`, `pname`, `title`, `brand`, `description`, `price`, `
 --
 
 CREATE TABLE `scat` (
-  `scid` bigint(20) UNSIGNED NOT NULL,
-  `scname` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Dumping data for table `scat`
---
-
-INSERT INTO `scat` (`scid`, `scname`) VALUES
-(1, 'abc');
+  `scid` int(10) NOT NULL,
+  `cid` int(10) NOT NULL,
+  `scname` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -199,38 +96,32 @@ INSERT INTO `scat` (`scid`, `scname`) VALUES
 --
 
 CREATE TABLE `users` (
-  `uid` bigint(20) UNSIGNED NOT NULL,
-  `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `email` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `profile` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `selfie` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `location` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `email_verified_at` timestamp NULL DEFAULT NULL,
-  `password` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`uid`, `name`, `email`, `profile`, `selfie`, `location`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'Atharva', 'asdeshpande@mitaoe.ac.in', 'abc', 'xyz', 'pqr', NULL, '$2y$10$.dgOb9vBE7uig1xRdHn/lOhf0bQCq3oaz1D5digLBrxjMGmTMKxEO', NULL, '2020-08-16 11:46:56', '2020-08-16 11:46:56'),
-(2, 'AD', 'atharvadeshpande99@gmail.com', 'qqq', 'www', 'rrr', NULL, '$2y$10$qe6z1OxFyfnCpsDx.IUuluT2MmiApfKvDmhOsZ9qSaO528cWaHz4u', NULL, '2020-08-16 22:12:31', '2020-08-16 22:12:31');
+  `uid` int(10) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `pwd` varchar(50) NOT NULL,
+  `phone` int(20) NOT NULL,
+  `loc` varchar(200) NOT NULL,
+  `photo` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `buyers`
+-- Indexes for table `adimgs`
 --
-ALTER TABLE `buyers`
-  ADD KEY `buyers_pid_foreign` (`pid`),
-  ADD KEY `buyers_sid_foreign` (`sid`),
-  ADD KEY `buyers_bid_foreign` (`bid`);
+ALTER TABLE `adimgs`
+  ADD KEY `fk_aid` (`aid`);
+
+--
+-- Indexes for table `ads`
+--
+ALTER TABLE `ads`
+  ADD PRIMARY KEY (`aid`),
+  ADD KEY `fk_uid` (`uid`),
+  ADD KEY `fk_scid` (`scid`);
 
 --
 -- Indexes for table `cat`
@@ -239,138 +130,80 @@ ALTER TABLE `cat`
   ADD PRIMARY KEY (`cid`);
 
 --
--- Indexes for table `chat`
+-- Indexes for table `locations`
 --
-ALTER TABLE `chat`
-  ADD KEY `chat_sid_foreign` (`sid`),
-  ADD KEY `chat_rid_foreign` (`rid`);
-
---
--- Indexes for table `failed_jobs`
---
-ALTER TABLE `failed_jobs`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `fav`
---
-ALTER TABLE `fav`
-  ADD KEY `fav_pid_foreign` (`pid`),
-  ADD KEY `fav_uid_foreign` (`uid`);
-
---
--- Indexes for table `migrations`
---
-ALTER TABLE `migrations`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `postimg`
---
-ALTER TABLE `postimg`
-  ADD KEY `postimg_pid_foreign` (`pid`);
-
---
--- Indexes for table `posts`
---
-ALTER TABLE `posts`
-  ADD PRIMARY KEY (`pid`),
-  ADD KEY `posts_uid_foreign` (`uid`),
-  ADD KEY `posts_cid_foreign` (`cid`),
-  ADD KEY `posts_scid_foreign` (`scid`);
+ALTER TABLE `locations`
+  ADD KEY `fk_aid_loc` (`aid`);
 
 --
 -- Indexes for table `scat`
 --
 ALTER TABLE `scat`
-  ADD PRIMARY KEY (`scid`);
+  ADD PRIMARY KEY (`scid`),
+  ADD KEY `fk_cid` (`cid`);
 
 --
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`uid`),
-  ADD UNIQUE KEY `users_email_unique` (`email`);
+  ADD PRIMARY KEY (`uid`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
+-- AUTO_INCREMENT for table `ads`
+--
+ALTER TABLE `ads`
+  MODIFY `aid` int(10) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `cat`
 --
 ALTER TABLE `cat`
-  MODIFY `cid` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT for table `failed_jobs`
---
-ALTER TABLE `failed_jobs`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `migrations`
---
-ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
-
---
--- AUTO_INCREMENT for table `posts`
---
-ALTER TABLE `posts`
-  MODIFY `pid` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `cid` int(10) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `scat`
 --
 ALTER TABLE `scat`
-  MODIFY `scid` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `scid` int(10) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `uid` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `uid` int(10) NOT NULL AUTO_INCREMENT;
 
 --
 -- Constraints for dumped tables
 --
 
 --
--- Constraints for table `buyers`
+-- Constraints for table `adimgs`
 --
-ALTER TABLE `buyers`
-  ADD CONSTRAINT `buyers_bid_foreign` FOREIGN KEY (`bid`) REFERENCES `users` (`uid`) ON DELETE CASCADE,
-  ADD CONSTRAINT `buyers_pid_foreign` FOREIGN KEY (`pid`) REFERENCES `posts` (`pid`) ON DELETE CASCADE,
-  ADD CONSTRAINT `buyers_sid_foreign` FOREIGN KEY (`sid`) REFERENCES `users` (`uid`) ON DELETE CASCADE;
+ALTER TABLE `adimgs`
+  ADD CONSTRAINT `fk_aid` FOREIGN KEY (`aid`) REFERENCES `ads` (`aid`);
 
 --
--- Constraints for table `chat`
+-- Constraints for table `ads`
 --
-ALTER TABLE `chat`
-  ADD CONSTRAINT `chat_rid_foreign` FOREIGN KEY (`rid`) REFERENCES `users` (`uid`) ON DELETE CASCADE,
-  ADD CONSTRAINT `chat_sid_foreign` FOREIGN KEY (`sid`) REFERENCES `users` (`uid`) ON DELETE CASCADE;
+ALTER TABLE `ads`
+  ADD CONSTRAINT `fk_scid` FOREIGN KEY (`scid`) REFERENCES `scat` (`scid`),
+  ADD CONSTRAINT `fk_uid` FOREIGN KEY (`uid`) REFERENCES `users` (`uid`);
 
 --
--- Constraints for table `fav`
+-- Constraints for table `locations`
 --
-ALTER TABLE `fav`
-  ADD CONSTRAINT `fav_pid_foreign` FOREIGN KEY (`pid`) REFERENCES `posts` (`pid`) ON DELETE CASCADE,
-  ADD CONSTRAINT `fav_uid_foreign` FOREIGN KEY (`uid`) REFERENCES `users` (`uid`) ON DELETE CASCADE;
+ALTER TABLE `locations`
+  ADD CONSTRAINT `fk_aid_loc` FOREIGN KEY (`aid`) REFERENCES `ads` (`aid`);
 
 --
--- Constraints for table `postimg`
+-- Constraints for table `scat`
 --
-ALTER TABLE `postimg`
-  ADD CONSTRAINT `postimg_pid_foreign` FOREIGN KEY (`pid`) REFERENCES `posts` (`pid`) ON DELETE CASCADE;
-
---
--- Constraints for table `posts`
---
-ALTER TABLE `posts`
-  ADD CONSTRAINT `posts_cid_foreign` FOREIGN KEY (`cid`) REFERENCES `cat` (`cid`) ON DELETE CASCADE,
-  ADD CONSTRAINT `posts_scid_foreign` FOREIGN KEY (`scid`) REFERENCES `scat` (`scid`) ON DELETE CASCADE,
-  ADD CONSTRAINT `posts_uid_foreign` FOREIGN KEY (`uid`) REFERENCES `users` (`uid`) ON DELETE CASCADE;
+ALTER TABLE `scat`
+  ADD CONSTRAINT `fk_cid` FOREIGN KEY (`cid`) REFERENCES `cat` (`cid`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
